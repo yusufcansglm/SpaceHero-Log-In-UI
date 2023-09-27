08c391cd30a951cf0ee4151eff5a3257439c1ea5
Yusuf Can Sağlam <yusufcan.sglm@hotmail.com>
2023-09-27 21:29:41 +0300

    Initial Commit

---
+++ Hero/HeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroApp.swift
+//  Hero
+//
+//  Created by Yusuf Can Sağlam on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
